46321c196d88fd79deda3ed835ad5023acc5b168
xnzar <mustafacetin2043@gmail.com>
2024-05-13 14:53:46 +0300

    class-add

---
+++ src/main/java/com/mustafa/spring/jwt/model/Role.java
@@ -0,0 +1,6 @@
+package com.mustafa.spring.jwt.model;
+
+public enum Role {
+    USER,
+    ADMIN
+}
---
+++ src/main/java/com/mustafa/spring/jwt/service/UserDetailsImpl.java
@@ -0,0 +1,20 @@
+package com.mustafa.spring.jwt.service;
+
+import com.mustafa.spring.jwt.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+    public UserDetailsImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUserName(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found."));
+    }
+}
